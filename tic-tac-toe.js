10df1f319505d2e139b8640985773c69b22e04ff
ahmetkocar <ahmetkocar@gmail.com>
2023-03-25 16:17:03 +0300

    Create basic gameplay

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,47 +1,104 @@
-let gameboard = [
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-];
+const gameBoard = (() => {
+  const cells = document.querySelectorAll(".cell");
+  let squares = [
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+  ];
+  let turn = "X";
 
-let turn = "X";
-const playground = document.querySelector(".playground");
-const cells = document.querySelectorAll(".cell");
-
-function renderGameboard(gameboard) {
-  for (let i = 0; i < gameboard.length; i++) {
-    if (gameboard[i] !== undefined && cells[i].innerHTML === "") {
-      let mark = document.createTextNode(`${gameboard[i]}`);
-      cells[i].appendChild(mark);
+  function mark(e) {
+    if (turn === "X") {
+      squares[Array.from(cells).indexOf(e.target)] = "X";
+      renderGameBoard(squares);
+      if (checkWinner()) {
+        for (let i = 0; i < cells.length; i++) {
+          cells[i].removeEventListener("click", mark);
+        }
+      } else {
+        turn = "O";
+        e.target.removeEventListener("click", mark);
+      }
+    } else {
+      squares[Array.from(cells).indexOf(e.target)] = "O";
+      renderGameBoard(squares);
+      if (checkWinner()) {
+        for (let i = 0; i < cells.length; i++) {
+          cells[i].removeEventListener("click", mark);
+        }
+      } else {
+        turn = "X";
+        e.target.removeEventListener("click", mark);
+      }
     }
   }
-}
 
-renderGameboard(gameboard);
-
-function markX(e) {
-  gameboard[Array.from(cells).indexOf(e.target)] = "X";
-  renderGameboard(gameboard);
-  turn = "O";
-}
-
-function markO(e) {
-  gameboard[Array.from(cells).indexOf(e.target)] = "O";
-  renderGameboard(gameboard);
-  turn = "X";
-}
-for (let i = 0; i < cells.length; i++) {
-  cells[i].addEventListener("click", function (e) {
-    if (turn === "X") {
-      markX(e);
-    } else {
-      markO(e);
+  function renderGameBoard(squares) {
+    for (let i = 0; i < squares.length; i++) {
+      if (squares[i] !== undefined && cells[i].innerHTML === "") {
+        let mark = document.createTextNode(`${squares[i]}`);
+        cells[i].appendChild(mark);
+      }
     }
-  });
-}
+  }
+
+  function checkWinner() {
+    // column check
+    for (let i = 0; i < 3; i++) {
+      if (
+        gameBoard.squares[i] !== undefined &&
+        gameBoard.squares[i] === gameBoard.squares[i + 3] &&
+        gameBoard.squares[i] === gameBoard.squares[i + 6]
+      ) {
+        return true;
+      }
+    }
+    // row check
+    for (let i = 0; i < 7; i += 3) {
+      if (
+        gameBoard.squares[i] !== undefined &&
+        gameBoard.squares[i] === gameBoard.squares[i + 1] &&
+        gameBoard.squares[i] === gameBoard.squares[i + 2]
+      ) {
+        return true;
+      }
+    }
+    // diagonal checks
+    if (
+      gameBoard.squares[0] !== undefined &&
+      gameBoard.squares[0] === gameBoard.squares[4] &&
+      gameBoard.squares[0] === gameBoard.squares[8]
+    ) {
+      return true;
+    }
+    if (
+      gameBoard.squares[2] !== undefined &&
+      gameBoard.squares[2] === gameBoard.squares[4] &&
+      gameBoard.squares[2] === gameBoard.squares[6]
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener("click", mark);
+  }
+
+  return { squares, turn, mark };
+})();
+
+// const player = (name, marker) => {
+//   return { name, marker };
+// };
+
+const gamePlay = (() => {
+  // const player1 = player("Player 1", "X");
+  // const player2 = player("Player 2", "O");
+})();
